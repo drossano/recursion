b01ce85eda420b6351df2b05c3677215562f527c
Dean <72749793+drossano@users.noreply.github.com>
2022-02-03 16:24:53 -0500

    Dipslay 0 for first fibonacci number

--- fibonacci/fib.rb
+++ fibonacci/fib.rb
@@ -1,17 +1,19 @@
-require 'pry-byebug'
 def fibs(n)
   arr = []
   i = 0
   until i == n
-    if i == 0 || i == 1
-      arr.push(1)
+    if i == 0
+      arr.push(0)
       i += 1
+    elsif i == 1
+      arr.push(1)
+       i += 1 
     else
       arr.push(arr[i - 1] + arr[i - 2])
       i += 1
     end
   end
-  p arr
+  arr
 end
 
-fibs(8)
+p fibs(8)
