75d8d3ee8950aff9dc414aa4a9dd70c4d72b6f34
Dean <72749793+drossano@users.noreply.github.com>
2022-02-04 10:49:39 -0500

    Add build_fib_array and fibs_rec methods

--- fibonacci/fib.rb
+++ fibonacci/fib.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 def fibs(n)
   arr = []
   i = 0
@@ -7,7 +8,7 @@ def fibs(n)
       i += 1
     elsif i == 1
       arr.push(1)
-       i += 1 
+      i += 1
     else
       arr.push(arr[i - 1] + arr[i - 2])
       i += 1
@@ -16,4 +17,20 @@ def fibs(n)
   arr
 end
 
-p fibs(8)
+def fibs_rec(n)
+  if n == 1
+    0
+  elsif n == 2
+    1
+  else
+    fibs_rec(n - 1) + fibs_rec(n - 2)
+  end
+end
+
+def build_fib_array(n)
+  arr = (1..n).to_a
+  arr.map { |element| fibs_rec(element) }
+end
+
+p fibs(9)
+p build_fib_array(9)
